1de0179b821e671ab59782d542299cdb12488867
Sonrisa <sonrisa722611@gmail.com>
2017-12-25 22:29:41 -0800

    Update ctci1.4

--- CTCI/arrayAndStrings/1-4palindromePermutation.js
+++ CTCI/arrayAndStrings/1-4palindromePermutation.js
@@ -1,3 +1,22 @@
-const palindromPermutation = () => {};
+const palindromPermutation = input => {
+  let palindromObj = {};
+  let oddNumerCounter = 0;
+  for (letter of input) {
+    letter = letter.toLowerCase();
+    if (palindromObj[letter]) {
+      palindromObj[letter]++;
+    } else {
+      palindromObj[letter] = 1;
+    }
+  }
 
-console.log(palindromPermutation('Tact Coa') === true);
+  for (key in palindromObj) {
+    if (palindromObj[key] % 2 !== 0 && key !== ' ') {
+      oddNumerCounter++;
+    }
+  }
+  return oddNumerCounter > 1 ? false : true;
+};
+
+// palindromPermutation('Tact Coa');
+// console.log(palindromPermutation('Tact Coa') === true); // tac
