aa98dea115c162c69e82fb2f89f092821eecdd0f
Sonrisa <sonrisa722611@gmail.com>
2018-01-10 14:52:56 -0800

    Update waterBlocks with finding two walls

---
+++ stagingDay/waterBlock2ndTry.js
@@ -0,0 +1,9 @@
+const waterBlocks = function(blocks) {};
+
+const input = [5, 2, 3, 2, 1, 3];
+const output = waterBlocks(input);
+console.log(output);
+
+const input2 = [1, 2, 3, 2, 1];
+const output2 = waterBlocks(input2);
+console.log(output2);
--- stagingDay/waterBlocks.js
+++ stagingDay/waterBlocks.js
@@ -1,31 +1,29 @@
 const waterBlocks = function(blocks) {
   function findTempBucket(start, end) {
     // find left block
-    while (blocks[start] > blocks[start - 1] && blocks[start] > 0) {
+    while (blocks[start] < blocks[start - 1] && blocks[start] > 0) {
       start--;
     }
     // find right block
-    while (blocks[end] < blocks[end + 1] && blocks[end] < blocks.length) {
+    while (blocks[end] > blocks[end + 1] && blocks[end] < blocks.length) {
       end++;
     }
-
-    return [start, end];
+    return [start, end + 1];
   }
 
   function findDrops(inputBucket) {
-    console.log('inputBucket', inputBucket);
+    let oneDrop = 0;
+    if (inputBucket[0] - inputBucket[1] === 0) return oneDrop;
     let leftWallValue = blocks[inputBucket[0]];
     let rightWallValue = blocks[inputBucket[1]];
-    console.log('leftWallValue', leftWallValue);
-    console.log('rightWallValue', rightWallValue);
-    let oneDrop = 0;
+
     let tempMin =
       leftWallValue > rightWallValue ? rightWallValue : leftWallValue;
-    console.log('tempMin', tempMin);
+
     for (let i = inputBucket[0] + 1; i < inputBucket[1]; i++) {
       oneDrop += tempMin - blocks[i];
     }
-    console.log('oneDrop', oneDrop);
+
     return oneDrop;
   }
 
@@ -33,26 +31,20 @@ const waterBlocks = function(blocks) {
   let result = 0;
   let leftWallIndex = 0;
   let rightWallIndex = 1;
-  console.log('35', leftWallIndex);
+
   while (rightWallIndex < blocks.length) {
-    console.log('36', leftWallIndex);
     let tempBucket = findTempBucket(leftWallIndex, rightWallIndex);
     let findedDrops = findDrops(tempBucket);
 
     leftWallIndex = tempBucket[0];
     rightWallIndex = tempBucket[1];
 
-    if (leftWallIndex === 0 || rightWallIndex === blocks.length - 1) {
-      findedDrops = 0;
-    }
-    console.log('findedDrops', findedDrops);
     result += findedDrops;
-    console.log('rightWallIndex', rightWallIndex);
+    if (rightWallIndex >= blocks.length - 1) return result;
+
     leftWallIndex = rightWallIndex;
-    console.log('leftWallIndex', leftWallIndex);
     rightWallIndex = leftWallIndex + 1;
   }
-  return result;
 };
 
 const input = [5, 2, 3, 2, 1, 3];
