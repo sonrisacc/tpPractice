9afd2e4a56a2fbd0c28579e7c8711a1120a3ece7
Sonrisa <sonrisa722611@gmail.com>
2018-01-21 08:19:41 -0800

    Update mergeIntervals

--- round-robin/mergeIntervals.js
+++ round-robin/mergeIntervals.js
@@ -17,36 +17,19 @@ const mergeInterval = function(arr) {
   return result;
 };
 
-const input = [
-  { start: 2, end: 6 },
-  { start: 0, end: 5 },
-  { start: 3, end: 7 }
-];
-const input1 = [];
-const input2 = [{ start: 0, end: 7 }];
-
-const output = mergeInterval(input);
-const output1 = mergeInterval(input1);
-const output2 = mergeInterval(input2);
-
-console.log(output);
-console.log(output1);
-console.log(output2);
-
-// create a copy of the arr
-// deal with edge
-// sort list of arr
-
-// iterate through the list,
-// if inputLength <= 1 return input
-// compare cur with next interval
-// if cur end > next start
-// newEnd = math.max(curEnd, with nextEnd)
-// start = curStart
-// push this newIntercal to result as [start, newEnd]
-// if cur end < next start
-// push cur interval to result
-// return result
-
-// edge condition:
 //
+// const input = [
+//   { start: 2, end: 6 },
+//   { start: 0, end: 5 },
+//   { start: 3, end: 7 }
+// ];
+// const input1 = [];
+// const input2 = [{ start: 0, end: 7 }];
+//
+// const output = mergeInterval(input);
+// const output1 = mergeInterval(input1);
+// const output2 = mergeInterval(input2);
+//
+// console.log(output);
+// console.log(output1);
+// console.log(output2);
